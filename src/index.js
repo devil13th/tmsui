25d4ac93a383db30bbfab044ed52422f0bcf603c
wang lei <wanglei62@lenovo.com>
2020-09-23 09:17:19 +0800

    eject 后加入env 和 alias

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '@/App';
 import * as serviceWorker from './serviceWorker';
+
+alert(process.env.REACT_APP_NAME)
 
 ReactDOM.render(
   <React.StrictMode>
