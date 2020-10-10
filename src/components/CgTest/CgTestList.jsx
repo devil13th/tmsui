9858f434137ad1164f2a36dd7f0f37453f60a39c
wang lei <wanglei62@lenovo.com>
2020-10-10 23:34:21 +0800

    Update CgTestList.jsx

--- src/components/CgTest/CgTestList.jsx
+++ src/components/CgTest/CgTestList.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Menu, Table, Button, Form, Row, Col, Input } from 'antd'
 
 class CgTestList extends React.Component {
   // constructor(props) {
   //   super(props)
-  //   this.state = {}
-  // }
+  //   this.state = {
 
+  //   }
+  // }
   state = {
-    selectedRowKeys: [1, 2, 3], // Check here to configure the default column
-    loading: false,
+    tableSelectedRowKeys: [1, 2, 3], // Check here to configure the default column
+    // 表格loading
+    tableLoading: false,
+    // 表格数据
+    data: [],
   }
 
   componentDidMount = () => {
-    this.setState({ loading: true })
+    this.setState({ tableLoading: true })
+
+    const data = []
+    for (let i = 0; i < 46; i++) {
+      data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+      })
+    }
+
+    this.setState({ data })
     // ajax request after empty completing
     setTimeout(() => {
       this.setState({
-        loading: false,
+        tableLoading: false,
       })
     }, 2000)
   }
 
-  onSelectChange = (selectedRowKeys) => {
-    console.log('selectedRowKeys changed: ', selectedRowKeys)
-    this.setState({ selectedRowKeys })
+  onSelectChange = (tableSelectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', tableSelectedRowKeys)
+    this.setState({ tableSelectedRowKeys })
   }
 
   render() {
-    const { loading, selectedRowKeys } = this.state
+    const { loading, tableSelectedRowKeys } = this.state
     const rowSelection = {
-      selectedRowKeys,
+      tableSelectedRowKeys,
       onChange: this.onSelectChange,
     }
-    const hasSelected = selectedRowKeys.length > 0
+    const hasSelected = tableSelectedRowKeys.length > 0
 
     const columns = [
       {
@@ -50,24 +66,14 @@ class CgTestList extends React.Component {
       },
     ]
 
-    const data = []
-    for (let i = 0; i < 46; i++) {
-      data.push({
-        key: i,
-        name: `Edward King ${i}`,
-        age: 32,
-        address: `London, Park Lane no. ${i}`,
-      })
-    }
-
     return (
       <div>
         <Table
           rowSelection={rowSelection}
           columns={columns}
-          dataSource={data}
+          dataSource={this.state.data}
           size={'small'}
-          loading={loading}
+          loading={this.state.tableLoading}
         />
       </div>
     )
